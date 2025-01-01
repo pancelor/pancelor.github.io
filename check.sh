6edfbd2572a7dc17567b92c690ea18b6b139d261
pancelor <pancelor@gmail.com>
2024-12-31 22:17:42 -0800

    try to fix image prefetch

--- check.sh
+++ check.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
 
 # check for errors with ejs bleeding through
-ag --html "&lt;%|%&gt;" docs
+# "!" for correct terminal status code
+! rg --type html -F "&lt;%|%&gt;" docs
